f5389fbc0d1e60db0ac16a0a1d6236765333a490
horikawa <horikawa@horikawa8240.research.bemax.ac.jp>
2011-06-23 16:44:11 +0900

    タイトル画面を追加

--- src/bemax/dropbomsforandroid/DropBomsForAndroidActivity.java
+++ src/bemax/dropbomsforandroid/DropBomsForAndroidActivity.java
@@ -1,10 +1,14 @@
 package bemax.dropbomsforandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.SurfaceView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 
-public class DropBomsForAndroidActivity extends Activity{
+public class DropBomsForAndroidActivity extends Activity implements OnTouchListener{
 	private GameView gameView;
 
 	/** Called when the activity is first created. */
@@ -12,9 +16,10 @@ public class DropBomsForAndroidActivity extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.gameplay);
-        SurfaceView sView = (SurfaceView)findViewById(R.id.gameView);
-        gameView = new GameView(sView);
+        setContentView(R.layout.gametitle);
+
+        ImageView bom = (ImageView)findViewById(R.id.bom_image_view);
+        bom.setOnTouchListener(this);
     }
 
     @Override
@@ -35,5 +40,14 @@ public class DropBomsForAndroidActivity extends Activity{
     	// TODO 自動生成されたメソッド・スタブ
     	super.onDestroy();
     }
+
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(event.getAction()==MotionEvent.ACTION_DOWN){
+			Intent intent = new Intent(this, GamePlayActivity.class);
+			startActivity(intent);
+		}
+		return false;
+	}
 
 }
--- src/bemax/dropbomsforandroid/GamePlayActivity.java
+++ src/bemax/dropbomsforandroid/GamePlayActivity.java
@@ -1,7 +1,38 @@
 package bemax.dropbomsforandroid;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.SurfaceView;
 
 public class GamePlayActivity extends Activity {
+	private GameView gameView;
 
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.gameplay);
+        SurfaceView sView = (SurfaceView)findViewById(R.id.gameView);
+        gameView = new GameView(sView);
+    }
+
+    @Override
+    protected void onStart() {
+    	// TODO アクティビティが開始された
+    	super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+    	// TODO 自動生成されたメソッド・スタブ
+    	super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+    	// TODO 自動生成されたメソッド・スタブ
+    	super.onDestroy();
+    }
 }
